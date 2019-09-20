f6ab4d1869268e082b259f86f5b1ffede1bcfcba
gustavolopes899 <gustavolopes899@gmail.com>
2019-09-20 19:30:48 -0300

    Adicionando arquivos do projeto

---
+++ app/src/main/java/br/edu/ifsp/scl/calculadorasdmkt/view/MainActivity.kt
@@ -0,0 +1,23 @@
+package br.edu.ifsp.scl.calculadorasdmkt.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.calculadorasdmkt.R
+import kotlinx.android.synthetic.main.toolbar.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Toolbar
+        toolbar.title = getString(R.string.app_name)
+        setSupportActionBar(toolbar)
+
+        // Fragment padrão
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.calculadoraFl, CalculadoraBasicaFragment()).commit()
+    }
+
+}
